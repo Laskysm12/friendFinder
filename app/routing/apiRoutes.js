ddd7f2c3d449be85b8e5098865010b438b34506f
Sam Lasky <Laskysm@gmail.com>
2018-04-02 17:10:20 -0400

    HTML refinished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,7 @@ var friendsData = require('../data/friends.js');
 // Here "app" represents express
 module.exports = function (app) {
 
+    // GET route with the url /api/friends used to display a JSON of all possible friends
     app.get('/api/friends', function (req, res) {
         res.json(friendsData);
     })
